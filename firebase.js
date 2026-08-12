a6440753e9b09a8b00860ca6a1ee1338f279ea87
mpframes <mpframes1109@gmail.com>
2026-08-12 20:23:39 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,30 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDhnMP9qnXdhXAsIP6UIeOjuMz-5OVwdwE",
-  authDomain: "mp-frames.firebaseapp.com",
-  projectId: "mp-frames",
-  storageBucket: "mp-frames.firebasestorage.app",
-  messagingSenderId: "1062760470249",
-  appId: "1:1062760470249:web:37604cd27f43438aff0278",
-  measurementId: "G-6GZ6PEEH3L"
+    apiKey: "AIzaSyDhnMP9qnXdhXAsIP6UIeOjuMz-5OVwdwE",
+    authDomain: "mp-frames.firebaseapp.com",
+    projectId: "mp-frames",
+    storageBucket: "mp-frames.firebasestorage.app",
+    messagingSenderId: "1062760470249",
+    appId: "1:1062760470249:web:37604cd27f43438aff0278",
+    measurementId: "G-6GZ6PEEH3L"
 };
 
+
 const app = initializeApp(firebaseConfig);
+
 const db = getFirestore(app);
 
-export { app, db };
+const storage = getStorage(app);
+
+
+export {
+    app,
+    db,
+    storage
+};
