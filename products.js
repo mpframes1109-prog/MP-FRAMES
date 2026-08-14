019e022e198afe061a42a7697919de595a2ac916
mpframes <mpframes1109@gmail.com>
2026-08-14 23:27:57 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,26 +1,45 @@
 const products = [
-  {
-    id: 1,
-    name: "Black Wooden Frame",
-    price: 499,
-    image: "images/frame1.jpg"
-  },
-  {
-    id: 2,
-    name: "White Photo Frame",
-    price: 399,
-    image: "images/frame2.jpg"
-  },
-  {
-    id: 3,
-    name: "Premium Golden Frame",
-    price: 799,
-    image: "images/frame3.jpg"
-  },
-  {
-    id: 4,
-    name: "Collage Frame",
-    price: 999,
-    image: "images/frame4.jpg"
-  }
+
+{
+id:1,
+name:"Black Wooden Frame",
+price:499,
+image:"images/frame1.jpg"
+},
+
+{
+id:2,
+name:"White Photo Frame",
+price:399,
+image:"images/frame2.jpg"
+},
+
+{
+id:3,
+name:"Golden Premium Frame",
+price:799,
+image:"images/frame3.jpg"
+},
+
+{
+id:4,
+name:"Collage Frame",
+price:999,
+image:"images/frame4.jpg"
+},
+
+{
+id:5,
+name:"Acrylic Frame",
+price:699,
+image:"images/frame5.jpg"
+},
+
+{
+id:6,
+name:"Wedding Frame",
+price:899,
+image:"images/frame6.jpg"
+}
+
 ];
