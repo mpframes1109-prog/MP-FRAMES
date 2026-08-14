5391a2383dad9fb4d5c9393a9c81360a9f014006
mpframes <mpframes1109@gmail.com>
2026-08-15 00:18:34 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,87 +1,21 @@
-// ==========================================
-// FIREBASE APP
-// ==========================================
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
-import { initializeApp } from
-    "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-
-
-// ==========================================
-// FIRESTORE
-// ==========================================
-
-import { getFirestore } from
-    "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-
-
-// ==========================================
-// FIREBASE STORAGE
-// ==========================================
-
-import { getStorage } from
-    "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
-
-
-// ==========================================
-// FIREBASE CONFIG
-// ==========================================
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-
-    apiKey:
-        "AIzaSyDhnMP9qnXdhXAsIP6UIeOjuMz-5OVwdwE",
-
-    authDomain:
-        "mp-frames.firebaseapp.com",
-
-    projectId:
-        "mp-frames",
-
-    storageBucket:
-        "mp-frames.firebasestorage.app",
-
-    messagingSenderId:
-        "1062760470249",
-
-    appId:
-        "1:1062760470249:web:37604cd27f43438aff0278",
-
-    measurementId:
-        "G-6GZ6PEEH3L"
-
+    apiKey: "AIzaSyDhnMP9qnXdhXAsIP6UIeOjuMz-5OVwdwE",
+    authDomain: "mp-frames.firebaseapp.com",
+    projectId: "mp-frames",
+    storageBucket: "mp-frames.firebasestorage.app",
+    messagingSenderId: "1062760470249",
+    appId: "1:1062760470249:web:37604cd27f43438aff0278",
+    measurementId: "G-6GZ6PEEH3L"
 };
 
+const app = initializeApp(firebaseConfig);
 
-// ==========================================
-// INITIALIZE FIREBASE
-// ==========================================
+const db = getFirestore(app);
 
-const app =
-    initializeApp(firebaseConfig);
-
-
-// ==========================================
-// INITIALIZE FIRESTORE
-// ==========================================
-
-const db =
-    getFirestore(app);
-
-
-// ==========================================
-// INITIALIZE STORAGE
-// ==========================================
-
-const storage =
-    getStorage(app);
-
-
-// ==========================================
-// EXPORT
-// ==========================================
-
-export {
-    app,
-    db,
-    storage
-};
+export { app, db };
