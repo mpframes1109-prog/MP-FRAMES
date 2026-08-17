ddd2a5b1fe7a7b57549fb05410ff42ac2ab380a5
mpframes <mpframes1109@gmail.com>
2026-08-17 23:05:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -49,4 +49,13 @@ function updateCartCount(){
 
 }
 
+function goBack() {
+
+    if (document.referrer) {
+        history.back();
+    } else {
+        window.location.href = "index.html";
+    }
+
+}
 updateCartCount();
